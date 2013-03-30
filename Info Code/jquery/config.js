220000ccbf297817066aaca19335f451fc4b77c3
Ronald Rivas Arguello <snakearguello@gmail.com>
2013-03-30 13:15:07 -0600

    star-commit

---
+++ Info Code/jquery/config.js
@@ -0,0 +1,10 @@
+$(document).ready (function)(){
+//Codigo que se Ejecuta cuando esta listo el Dom
+
+
+$("#ocultar").click(function(evento){
+    
+    //Aqui el codigo que pasara el evento
+    $("span3").css("display","none");
+});
+});
